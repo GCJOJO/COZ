b4322589b2e059de258901a3b093d3bbb610e5d9
GCJOJO <39382270+JOJOMARIO2007@users.noreply.github.com>
2020-07-01 13:23:41 +0200

    Added some tutorial assets + some IA stuffs + some stuffs

---
+++ Source/COZ/CustomFileWriter.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CustomFileWriter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COZ_API UCustomFileWriter : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
