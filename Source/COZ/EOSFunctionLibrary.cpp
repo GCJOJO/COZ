6843bbadb92be10f54cad64cd54142ac7c4861dc
GCJOJO <39382270+GCJOJO@users.noreply.github.com>
2020-08-29 14:38:43 +0200

    Trying to add EOS to COZ and more

--- Source/COZ/EOSFunctionLibrary.cpp
+++ Source/COZ/EOSFunctionLibrary.cpp
@@ -3,7 +3,14 @@
 
 #include "EOSFunctionLibrary.h"
 
-bool UEOSFunctionLibrary::EOSLoginPortal()
+// Engine Includes
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineIdentityInterface.h"
+
+// EOS Plugin Includes
+#include "OnlineSubsystemEOS.h"
+
+/*bool UEOSFunctionLibrary::EOSLoginPortal()
 {
 	return true;
 }
@@ -11,4 +18,25 @@ bool UEOSFunctionLibrary::EOSLoginPortal()
 bool UEOSFunctionLibrary::EOSLoginCode()
 {
 	return false;
+}*/
+
+bool UEOSFunctionLibrary::EOSRequestLogin(FString Id, FString Token, FString Type)
+{
+	IOnlineSubsystem* SubSystem = IOnlineSubsystem::Get(EOS_SUBSYSTEM);
+
+	if (SubSystem != nullptr)
+	{
+		IOnlineIdentityPtr OnlineIdentity = SubSystem->GetIdentityInterface();
+
+		if (OnlineIdentity != nullptr)
+		{
+			FOnlineAccountCredentials Credentials(Type, Id, Token);
+			return OnlineIdentity->Login(0, Credentials);
+		}
+	}
+
+	return false;
+
 }
+
+//https://www.epicgames.com/id/authorize?client_id=xyza7891RWJSJiXKnqTO3EYSRyRYmHVr&response_type=code&scope=basic_profile&redirect_uri=https:\\eos.xyza7891RWJSJiXKnqTO3EYSRyRYmHVr://epic/auth&state=rfGWJux2WL86Zxr6nKApCAnDo8KexEUE
